105ec96bccfdf125b5aeea113bb5bd52a7ab46aa
AniKanayo <micanian4real@gmail.com>
2023-07-04 21:58:22 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,12 +5,3 @@ export default function getListStudentIds(arr) {
 
   return arr.map((student) => student.id);
 }
-
-// Testing the function
-// const students = [
-//  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-//  { id: 2, firstName: 'James', location: 'Columbia' },
-//  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-// ];
-
-// console.log(getListStudentIds(students));
