2aee225e8ea523caf2f8f6639b6a78ca90f5b023
AniKanayo <micanian4real@gmail.com>
2023-08-21 22:56:40 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,9 +5,10 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+console.log('Welcome to Holberton School, what is your name?');
+
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
-  rl.close();
 });
 
 rl.on('close', () => {
