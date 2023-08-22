826efc7933ed1527af99b81bc35f14a498e1ebc9
AniKanayo <micanian4real@gmail.com>
2023-08-22 21:36:25 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,19 @@
-const readline = require('readline');
+// Write a welcome message to the standard output (console)
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// Listen for the 'readable' event on the standard input (keyboard input)
+process.stdin.on('readable', () => {
+  // Read the data from the standard input
+  const chunk = process.stdin.read();
+  // If the data is not null (i.e., the user has entered something)
+  if (chunk !== null) {
+    // Write the user's name to the standard output
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+// Listen for the 'end' event on the standard input (triggered when the user presses Ctrl+D)
+process.stdin.on('end', () => {
+  // Write a closing message to the standard output
+  process.stdout.write('This important software is now closing\n');
 });
-
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
-});
-
-module.exports = rl;
