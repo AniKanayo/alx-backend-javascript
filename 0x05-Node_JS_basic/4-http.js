0d7b0662b08c720026d10cb74af262e0b66e74cd
AniKanayo <micanian4real@gmail.com>
2023-08-22 22:44:52 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,11 +3,9 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
