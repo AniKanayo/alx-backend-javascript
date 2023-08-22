9c76ad38ecf5063984181d8420c6a62f947b972c
AniKanayo <micanian4real@gmail.com>
2023-08-22 21:11:18 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,14 +3,17 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: 'Welcome to Holberton School, what is your name?\n',
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+rl.prompt();
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
+rl.on('line', (line) => {
+  console.log(`Your name is: ${line}`);
+  rl.close();
+}).on('close', () => {
+  console.log('This important software is now closing\n');
+  process.exit(0);
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+module.exports = rl;
