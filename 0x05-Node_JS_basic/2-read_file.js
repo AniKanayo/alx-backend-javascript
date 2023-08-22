a0e968d303e5e3e97a3f00df8194012e9997112d
AniKanayo <micanian4real@gmail.com>
2023-08-22 22:15:25 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,37 @@
+// Import the file system module
 const fs = require('fs');
-const csv = require('csv-parser');
 
 function countStudents(path) {
-  // Check if the file exists
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
-
-  const students = [];
-  const fields = {};
-
-  // Read the file and parse the CSV data
-  fs.createReadStream(path)
-    .pipe(csv())
-    .on('data', (row) => {
-      // Ignore empty lines
-      if (row.field && row.firstname) {
-        students.push(row);
-        if (!fields[row.field]) {
-          fields[row.field] = [];
-        }
-        fields[row.field].push(row.firstname);
-      }
-    })
-    .on('end', () => {
-      console.log(`Number of students: ${students.length}`);
-      for (const field in fields) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
+  try {
+    // Read the file synchronously
+    const data = fs.readFileSync(path, 'utf8');
+    const result = [];
+    // Split the data by new lines and then by commas to get an array of arrays
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
     });
+    // Remove the first row (headers)
+    result.shift();
+    const newis = [];
+    // Create a new array with only the first and fourth columns (student name and field)
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    // Create a set of unique fields
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    // Initialize a counter for each field
+    fields.forEach((data) => { (final[data] = 0); });
+    // Count the number of students in each field
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    // Log the total number of students
+    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+    // Log the number of students in each field and a list of their names
+    Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+  } catch (E) {
+    // If an error occurs (e.g., the file does not exist), throw an error
+    throw Error('Cannot load the database');
+  }
 }
 
+// Export the function
 module.exports = countStudents;
