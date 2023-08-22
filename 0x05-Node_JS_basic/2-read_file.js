db11f5c2cab5c568be41443c389188f7c73bc4b0
AniKanayo <micanian4real@gmail.com>
2023-08-22 22:02:20 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,39 +1,34 @@
 const fs = require('fs');
-// eslint-disable-next-line no-unused-vars
-const path = require('path');
+const csv = require('csv-parser');
 
 function countStudents(path) {
-  try {
-    const filePath = path.resolve(__dirname, path);
-    const content = fs.readFileSync(filePath, 'utf-8');
-    const lines = content.split('\n').filter((line) => line.trim() !== '');
-
-    console.log(`Number of students: ${lines.length}`);
-
-    const headers = lines[0].split(',').map((field) => field.trim());
-    const students = lines.slice(1);
-
-    headers.forEach((header) => {
-      const fieldIndex = headers.indexOf(header);
-      const studentsInField = students.reduce((count, student) => {
-        const fields = student.split(',').map((field) => field.trim());
-        if (fields[fieldIndex] !== '') {
-          return count + 1;
-        }
-        return count;
-      }, 0);
-
-      const firstNames = students
-        .map((student) => student.split(',')[0].trim())
-        .filter((firstName) => firstName !== '');
-
-      console.log(`Number of students in ${header}: ${studentsInField}. List: ${firstNames.join(', ')}`);
-    });
-  } catch (error) {
-    console.error('Cannot load the database');
+  // Check if the file exists
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
   }
-}
 
-countStudents('database.csv');
+  const students = [];
+  const fields = {};
+
+  // Read the file and parse the CSV data
+  fs.createReadStream(path)
+    .pipe(csv())
+    .on('data', (row) => {
+      // Ignore empty lines
+      if (row.field && row.firstname) {
+        students.push(row);
+        if (!fields[row.field]) {
+          fields[row.field] = [];
+        }
+        fields[row.field].push(row.firstname);
+      }
+    })
+    .on('end', () => {
+      console.log(`Number of students: ${students.length}`);
+      for (const field in fields) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    });
+}
 
 module.exports = countStudents;
