af1ba5f6f2a8192519268b2175951e0a9bb2fbee
AniKanayo <micanian4real@gmail.com>
2023-08-25 18:34:42 +0000

    Unittests in JS

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,11 +1,15 @@
-const Utils = require('./utils');
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
 const sinon = require('sinon');
+
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with the correct arguments', () => {
     const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
-    // Rest of the test code
+    sendPaymentRequestToApi(100, 20);
 
     sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
 
@@ -15,7 +19,7 @@ describe('sendPaymentRequestToApi', () => {
   it('should log the correct message', () => {
     const consoleSpy = sinon.spy(console, 'log');
 
-    // Rest of the test code
+    sendPaymentRequestToApi(100, 20);
 
     sinon.assert.calledWith(consoleSpy, 'The total is: 10');
 
