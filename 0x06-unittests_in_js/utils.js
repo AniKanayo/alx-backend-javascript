e27010a363f74ee8a45e83d4e8d391942579c679
AniKanayo <micanian4real@gmail.com>
2023-08-25 00:45:25 +0000

    Unittests in JS

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,10 +1,18 @@
-// utils.js
 const Utils = {
   calculateNumber: function(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  if (type === 'SUM') {
     return roundedA + roundedB;
-  }
+  } else if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    if (roundedB === 0) {
+      return 'Error';
+    }
+    return roundedA / roundedB;
+  },
 };
 
-module.exports = Utils;
+module.exports = calculateNumber;
