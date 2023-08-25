d7d342ecdd0a5840920bf983a033006769637740
AniKanayo <micanian4real@gmail.com>
2023-08-25 19:29:23 +0000

    Unittests in JS

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,23 +6,26 @@ const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with the correct arguments', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-
-    sendPaymentRequestToApi(100, 20);
-
-    sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
-
-    calculateNumberStub.restore();
-  });
-
-  it('should log the correct message', () => {
+  it('should stub Utils.calculateNumber and log the correct message', () => {
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(120); // Update the stubbed return value
     const consoleSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledWith(consoleSpy, 'The total is: 10');
+    sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
+    sinon.assert.calledWith(consoleSpy, 'The total is: 120'); // Update the expected message
 
+    calculateNumberStub.restore();
     consoleSpy.restore();
   });
+
+//  it('should log the correct message', () => {
+//    const consoleSpy = sinon.spy(console, 'log');
+//
+//   sendPaymentRequestToApi(100, 20);
+//
+//    sinon.assert.calledWith(consoleSpy, 'The total is: 10');
+
+//    consoleSpy.restore();
+//  });
 });
