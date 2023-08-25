3d27d2eb4fb995b368fc7d0bda465326aee0be53
AniKanayo <micanian4real@gmail.com>
2023-08-25 01:40:04 +0000

    Unittests in JS

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,24 +4,17 @@ const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function() {
-  before(function() {
-    sinon.stub(Utils, 'calculateNumber').returns(10);
-  });
-
-  after(function() {
-    Utils.calculateNumber.restore();
+  beforeEach(function() {
+    sinon.restore();
   });
 
   it('should call Utils.calculateNumber with the correct arguments', function() {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    const consoleLogSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
     sinon.assert.calledWithExactly(calculateNumberSpy, 'SUM', 100, 20);
-    sinon.assert.calledWithExactly(consoleLogSpy, 'The total is: 10');
 
     calculateNumberSpy.restore();
-    consoleLogSpy.restore();
   });
 });
